385d151f3bc39ef6008da71ea4176298b78b0dc9
duanyunx <3367095003@qq.com>
2020-11-08 14:12:23 +0800

    这是第四周的项目文件内容

--- js/index.js
+++ js/index.js
@@ -12,8 +12,8 @@ $('#bannerInner').tyslide({
     isShow:true,//是否显示控制器
     isShowBtn:true,//是否显示左右按钮
     controltop:40,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
-    controlsW:25,//控制按钮宽度
-    controlsH:25,//控制按钮高度
+    controlsW:20,//控制按钮宽度
+    controlsH:20,//控制按钮高度
     radius:10,//控制按钮圆角度数
     controlsColor:"#d7d7d7",//普通控制按钮的颜色
     controlsCurrentColor:"#ff6600",//当前控制按钮的颜色
